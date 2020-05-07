5c8f2d6f6682441e6121a513fbae4f87c0f31aeb
Elvin Sellappan <60557928+elvins123@users.noreply.github.com>
2020-05-07 12:33:30 -0500

    Add files via upload

---
+++ MotionCameraEmailProgram.py
@@ -0,0 +1,57 @@
+from picamera import PiCamera
+import time
+import sys
+import time
+import random
+import datetime
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+
+
+
+pir = MotionSensor(4)
+
+camera = PiCamera()
+
+filename = "intruder.jpg"
+pir.wait_for_motion()
+print("Motion Detected")
+camera.capture(filename)
+time.sleep(1)
+pir.wait_for_no_motion()
+
+email_user = 'elsellan@gmail.com'
+email_password = ‘place your email password here’
+email_send = 'elsellan@gmail.com'
+
+subject = 'Python'
+
+msg = MIMEMultipart()
+msg['From'] = email_user
+msg['To'] = email_send
+msg['Subject'] = subject
+
+body = 'The Raspberry Pi has detected motion'
+msg.attach(MIMEText(body,'plain'))
+
+filename='intruder.jpg'
+attachment  =open(filename,'rb')
+
+part = MIMEBase('application','octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition',"attachment; filename= "+filename)
+
+msg.attach(part)
+text = msg.as_string()
+server = smtplib.SMTP('smtp.gmail.com',587)
+server.starttls()
+server.login(email_user,email_password)
+
+server.sendmail(email_user,email_send,text)
+server.quit()
+
+
---
+++ README.txt
@@ -0,0 +1,11 @@
+There are a couple things to note with this project:
+
+1. If you choose to modify the code, you must enter your own emails and password to your email in order to send the picture propertly
+
+2. Also, you must use GPIO 4 on the raspberry pi, as this project is configured with using that port
+
+3. You must have a raspberry pi camera that plugs into a camera slot, as this project is not configured for usb cameras
+
+Thank you and enjoy!
+
+Elvin
